1924bd0e384dd81825e1eb62b97edc060d033a56
Deadvlel <shadowboy812004@gmail.com>
2025-12-31 09:51:13 +0700

    api danh gia

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,7 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DanhGiaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/danh-gia', [DanhGiaController::class, 'store']);
+Route::get('/danh-gia/{maSanPham}', [DanhGiaController::class, 'index']);
