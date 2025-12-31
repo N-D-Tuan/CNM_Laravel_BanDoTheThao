f7374b3d9d72bca4e46dd137711a8477ad2fa9fb
Nguyen Thanh Phat <23042004phat@gmail.com>
2025-12-31 12:56:19 +0700

    api đơn hàng, chi tiết đơn hàng

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DonHangController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -7,3 +8,18 @@ use App\Http\Controllers\UserController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//Lấy toàn bộ đơn hàng cho admin
+Route::get('/donhang', [DonHangController::class, 'index']);
+
+//tạo đơn hàng
+Route::post('/donhang', [DonHangController::class, 'store']);
+
+//lấy đơn hàng theo user
+Route::get('/donhang/user/{id}', [DonHangController::class, 'getByUser']);
+
+//lấy đơn hàng theo mã đơn hàng
+Route::get('/donhang/{id}', [DonHangController::class, 'getByMaDonHang']);
+
+// Cập nhật trạng thái đơn hàng
+Route::put('/donhang/{id}/trangthai', [DonHangController::class, 'updateTrangThai']);
